bfd7788835e6a9125095f3e6db3cec35de772f72
Guru18650 <guruwszystkiego@gmail.com>
2024-02-22 20:13:36 +0100

    main

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+
+
+
 
 function App() {
+
+  const [length, setLength] = useState(25);
+  const handleLength = (event) => setLength(event.target.value)
+  const [mlitery, setMlitery] = useState(false);
+  const handleMlitery = (event) => setMlitery(event.target.checked)
+  const [dlitery, setDlitery] = useState(false);
+  const handleDlitery = (event) => setDlitery(event.target.checked)
+  const [liczby, setLiczby] = useState(false);
+  const handleLiczby = (event) => setLiczby(event.target.checked)
+  const [znaki, setZnaki] = useState(false);
+  const handleZnaki = (event) => setZnaki(event.target.checked)
+
+  const [haslo, setHaslo] = useState("");
+  
+  const mLiteryB = "qwertyuioplkjhgfdsazxcvbnm";
+  const dLiteryB = "QWERTYUIOPLKJHGFDSAZXCVBNM";
+  const liczbyB = "1234567890";
+  const znakiB = "!@#$%^&*()";
+
+  const [hasla, setHasla] = useState([]);
+  let baza = "";
+
+  const handleForm = () => {
+    let hasloTemp = "";
+    if(mlitery)
+      baza+=mLiteryB;
+    if(dlitery)
+      baza+=dLiteryB;
+    if(liczby)
+      baza+=liczbyB;
+    if(znaki)
+      baza+=znakiB;
+
+    for (let i = 0; i < length; i++) {
+      hasloTemp+=baza[Math.floor(Math.random() * baza.length)];
+    }
+    setHasla([...hasla, hasloTemp]);
+  };
+
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <img className='bg' src="/bg.jpg" alt='bg'/>
+      <div className='content'>
+      <h1>Generator haseł</h1>
+      <div className='center'>    
+      <label>Długość</label><br></br>
+    <input type="range" min="4" max="25"  onChange={handleLength}/>{length}<br></br>
+      <label className='form-control'>
+    <input type="checkbox" onChange={handleMlitery}/>
+    Małe Litery</label>
+    <label className='form-control'> 
+    <input type="checkbox" onChange={handleDlitery} />
+      Duże Litery</label>
+    <label className='form-control'>
+    <input type="checkbox" onChange={handleLiczby}/>
+      Liczby</label>
+    <label className='form-control'>
+    <input type="checkbox" onChange={handleZnaki}/>
+      Znaki specjalne</label>
+      </div>
+      <button onClick={handleForm} disabled={!(znaki||mlitery||dlitery||liczby)}>Wygeneruj</button>
+      <ul>
+        {hasla.map(haslo => (
+          <li onClick={()=>{setHasla(hasla.filter(a=>a!==haslo))}}>{haslo}</li>
+        ))}
+      </ul>
+  </div></div>
+  
   );
 }
 
